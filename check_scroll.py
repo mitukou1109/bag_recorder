0a9d9b36d58f037cf7960880649cf40ccac131c6
mitukou1109 <mitukou1109@gmail.com>
2024-10-16 21:44:07 +0900

    fix CheckScroll class to work properly

--- check_scroll.py
+++ check_scroll.py
@@ -1,31 +1,21 @@
 import bullet
-from bullet.charDef import *
+import bullet.charDef
+
 
 @bullet.keyhandler.init
 class CheckScroll:
     def __init__(
         self,
-        prompt: str = "",
-        choices: list = [],
-        check: str = "√",
-        check_color: str = bullet.colors.foreground["default"],
-        check_on_switch: str = bullet.colors.REVERSE,
-        word_color: str = bullet.colors.foreground["default"],
-        word_on_switch: str = bullet.colors.REVERSE,
-        background_color: str = bullet.colors.background["default"],
-        background_on_switch: str = bullet.colors.REVERSE,
-        pad_right=0,
-        indent: int = 0,
-        align=0,
-        margin: int = 0,
-        shift: int = 0,
-        height=None,
-        return_index: bool = False,
+        *,
+        prompt: str,
+        choices: list,
+        check: str,
+        align: int,
+        margin: int,
+        height: int,
     ):
         if not choices:
             raise ValueError("Choices cannot be empty!")
-        if indent < 0:
-            raise ValueError("Indent must be >= 0!")
         if margin < 0:
             raise ValueError("Margin must be >= 0!")
 
@@ -33,99 +23,127 @@ class CheckScroll:
         self.choices = choices
         self.checked = [False] * len(self.choices)
         self.pos = 0
-        self.indent = indent
+
         self.align = align
         self.margin = margin
-        self.shift = shift
+        self.up_indicator = "↑ ↑ ↑"
+        self.down_indicator = "↓ ↓ ↓"
+
         self.check = check
-        self.check_color = check_color
-        self.check_on_switch = check_on_switch
-        self.word_color = word_color
-        self.word_on_switch = word_on_switch
-        self.background_color = background_color
-        self.background_on_switch = background_on_switch
-        self.pad_right = pad_right
-        self.max_width = len(max(self.choices, key=len)) + self.pad_right
-        self.height = min(len(self.choices), height if height else len(self.choices))
+
+        self.max_width = len(max(self.choices, key=len))
+        self.height = min(len(self.choices), height)
+
         self.top = 0
-        self.return_index = return_index
 
     def renderRows(self):
-        self.printRow(self.top, indicator="↑" if self.top != 0 else "")
+        self.printBorder(indicator=self.up_indicator if self.top != 0 else "")
         bullet.utils.forceWrite("\n")
-        for i in range(self.top + 1, self.top + self.height - 1):
+        bottom = self.top + self.height
+        for i in range(self.top, bottom):
             self.printRow(i)
             bullet.utils.forceWrite("\n")
-        if self.top + self.height - 1 < len(self.choices):
-            self.printRow(self.top + self.height - 1, indicator="↓")
+        self.printBorder(
+            indicator=self.down_indicator if bottom < len(self.choices) else ""
+        )
 
-    def printRow(self, idx, indicator=""):
-        bullet.utils.forceWrite(" " * (self.indent + self.align))
-        back_color = (
-            self.background_on_switch if idx == self.pos else self.background_color
+    def printRow(self, idx):
+        bullet.utils.forceWrite(" " * self.align)
+        bg_color = (
+            bullet.colors.REVERSE
+            if idx == self.pos
+            else bullet.colors.background["default"]
         )
-        word_color = self.word_on_switch if idx == self.pos else self.word_color
-        check_color = self.check_on_switch if idx == self.pos else self.check_color
+        fg_color = (
+            bullet.colors.REVERSE
+            if idx == self.pos
+            else bullet.colors.foreground["default"]
+        )
+
         if self.checked[idx]:
-            bullet.utils.cprint(self.check, check_color, back_color, end="")
+            bullet.utils.cprint(
+                self.check + " " * self.margin, fg_color, bg_color, end=""
+            )
         else:
-            bullet.utils.cprint(" ", check_color, back_color, end="")
+            bullet.utils.cprint(
+                " " * (len(self.check) + self.margin), fg_color, bg_color, end=""
+            )
+        bullet.utils.cprint(self.choices[idx], fg_color, bg_color, end="")
         bullet.utils.cprint(
-            " " * self.margin + self.choices[idx], word_color, back_color, end=""
-        )
-        bullet.utils.cprint(
-            " " * (self.max_width - len(self.choices[idx])), on=back_color, end=""
-        )
-        bullet.utils.cprint(
-            indicator, color=bullet.colors.foreground["default"], end=""
+            " " * (self.max_width - len(self.choices[idx])), on=bg_color, end=""
         )
         bullet.utils.moveCursorHead()
 
-    @bullet.keyhandler.register(SPACE_CHAR)
+    def printBorder(self, indicator=""):
+        bullet.utils.forceWrite(" " * (self.align + len(self.check) + self.margin))
+        bullet.utils.cprint(indicator, end="")
+        bullet.utils.moveCursorHead()
+
+    @bullet.keyhandler.register(bullet.charDef.SPACE_CHAR)
     def toggleRow(self):
         self.checked[self.pos] = not self.checked[self.pos]
-        self.renderRows()
-        bullet.utils.moveCursorUp(self.height)
+        self.printRow(self.pos)
 
-    @bullet.keyhandler.register(ARROW_UP_KEY)
+    @bullet.keyhandler.register(bullet.charDef.ARROW_UP_KEY)
     def moveUp(self):
-        if self.pos > 0:
+        if self.pos == self.top:
+            if self.top == 0:
+                return
+            else:
+                bullet.utils.moveCursorUp(1)
+                bullet.utils.clearConsoleDown(self.height + 1)
+                self.pos, self.top = self.pos - 1, self.top - 1
+                self.renderRows()
+                bullet.utils.moveCursorUp(self.height)
+        else:
+            bullet.utils.clearLine()
             self.pos -= 1
-            if self.pos < self.top:
-                self.top -= 1
-            self.renderRows()
-            bullet.utils.moveCursorUp(self.height)
+            self.printRow(self.pos + 1)
+            bullet.utils.moveCursorUp(1)
+            self.printRow(self.pos)
 
-    @bullet.keyhandler.register(ARROW_DOWN_KEY)
+    @bullet.keyhandler.register(bullet.charDef.ARROW_DOWN_KEY)
     def moveDown(self):
-        if self.pos < len(self.choices) - 1:
+        if self.pos == self.top + self.height - 1:
+            if self.top + self.height == len(self.choices):
+                return
+            else:
+                bullet.utils.moveCursorDown(1)
+                bullet.utils.clearConsoleUp(self.height + 2)
+                bullet.utils.moveCursorDown(1)
+                self.pos, self.top = self.pos + 1, self.top + 1
+                self.renderRows()
+                bullet.utils.moveCursorUp(1)
+        else:
+            bullet.utils.clearLine()
             self.pos += 1
-            if self.pos >= self.top + self.height:
-                self.top += 1
-            self.renderRows()
-            bullet.utils.moveCursorUp(self.height)
+            self.printRow(self.pos - 1)
+            bullet.utils.moveCursorDown(1)
+            self.printRow(self.pos)
 
-    @bullet.keyhandler.register(NEWLINE_KEY)
+    @bullet.keyhandler.register(bullet.charDef.NEWLINE_KEY)
     def accept(self):
-        bullet.utils.moveCursorDown(self.height - (self.pos - self.top))
-        ret = (
-            [i for i, checked in enumerate(self.checked) if checked]
-            if self.return_index
-            else [
-                choice for choice, checked in zip(self.choices, self.checked) if checked
-            ]
-        )
+        bullet.utils.moveCursorDown(self.top + self.height - self.pos + 1)
+        bullet.utils.forceWrite("\n")
+        ret = [self.choices[i] for i in range(len(self.choices)) if self.checked[i]]
         self.pos = 0
+        self.checked = [False] * len(self.choices)
         return ret
 
-    @bullet.keyhandler.register(INTERRUPT_KEY)
+    @bullet.keyhandler.register(bullet.charDef.INTERRUPT_KEY)
     def interrupt(self):
+        d = self.top + self.height - self.pos
+        bullet.utils.moveCursorDown(d)
         raise KeyboardInterrupt
 
     def launch(self):
         if self.prompt:
             bullet.utils.forceWrite(self.prompt + "\n")
         self.renderRows()
-        bullet.utils.moveCursorUp(self.height)
+        bullet.utils.forceWrite("\n")
+        bullet.utils.moveCursorUp(self.height + 1)
         with bullet.cursor.hide():
-            return self.accept()
+            while True:
+                ret = self.handle_input()
+                if ret is not None:
+                    return ret
